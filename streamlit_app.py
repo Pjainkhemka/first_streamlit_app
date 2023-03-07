a14ed17e8a14d6600eb335028ce1a225212a5507
Pjainkhemka <126957708+Pjainkhemka@users.noreply.github.com>
2023-03-07 12:20:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title ( ' My parents healthy new diner')
-
+streamlit.header ( ' Breakfast Menu ')
+streamlit.text ( ' Omega 2 and Blueberry Oatmeal ')
+streamlit.text (' Kale , Spinach , and Rocket Smoothie ')
+streamlit.text (' Hrad Boiled Free Range Egg')
